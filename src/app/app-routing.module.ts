e412dd1a7df85bb02b5d35ba9d2b26e2e79022ae
aya124707 <fawzyaya47@gmail.com>
2024-10-24 23:13:40 +0200

    events-cards

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -18,6 +18,10 @@ import { Page3Component } from './component/events/page3/page3.component';
 import { Page4Component } from './component/events/page4/page4.component';
 import { Page5Component } from './component/events/page5/page5.component';
 import { Page6Component } from './component/events/page6/page6.component';
+import { Page7Component } from './component/events/page7/page7.component';
+import { Page8Component } from './component/events/page8/page8.component';
+import { Page9Component } from './component/events/page9/page9.component';
+import { Page10Component } from './component/events/page10/page10.component';
 
 
 const routes: Routes = [
@@ -38,6 +42,10 @@ const routes: Routes = [
   {path: 'page4' ,component :Page4Component},
   {path: 'page5' ,component :Page5Component},
   {path: 'page6' ,component :Page6Component},
+  {path: 'page7' ,component :Page7Component},
+  {path: 'page8' ,component :Page8Component},
+  {path: 'page9' ,component :Page9Component},
+  {path: 'page10' ,component :Page10Component},
   {path: '**', component: NotFoundComponent },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -37,6 +37,10 @@ import { Page3Component } from './component/events/page3/page3.component';
 import { Page4Component } from './component/events/page4/page4.component';
 import { Page5Component } from './component/events/page5/page5.component';
 import { Page6Component } from './component/events/page6/page6.component';
+import { Page7Component } from './component/event-page/page7/page7.component';
+import { Page8Component } from './component/events/page8/page8.component';
+import { Page9Component } from './component/events/page9/page9.component';
+import { Page10Component } from './component/events/page10/page10.component';
 
 @NgModule({
   declarations: [
@@ -62,7 +66,7 @@ import { Page6Component } from './component/events/page6/page6.component';
     PipesComponent,
     EventPageComponent,
     UsersComponent, 
-    CreateAnEventComponent, Page1Component, Page2Component, Page3Component, Page4Component, Page5Component, Page6Component
+    CreateAnEventComponent, Page1Component, Page2Component, Page3Component, Page4Component, Page5Component, Page6Component, Page7Component, Page8Component, Page9Component, Page10Component
   ],
   imports: [
    BrowserModule,
